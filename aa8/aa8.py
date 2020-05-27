4a666e950dbdb1e122039a613e9f516b1efffc16
Alexandra <jenna.avis@gmail.com>
2020-05-27 18:56:38 +0300

    Fixed lab8

--- aa8/aa8.py
+++ aa8/aa8.py
@@ -2,14 +2,13 @@ import codecs
 from sklearn.tree import DecisionTreeClassifier
 from itertools import count
 import copy
+import time
 
 def freq_search(word, _dict):
     print('> Алгоритм частотного анализа')
-
     alph = [0]*32
     for i in range(len(_dict)):
             alph[ord(_dict[i][0][0]) - 1040] += 1
-
     keys = []
 
     for i in range(32):
@@ -17,7 +16,6 @@ def freq_search(word, _dict):
         if max(alph) != 0:
             alph[tmp] = 0
             keys.append(tmp)
-
     index = 0
 
     for i in range(len(keys)):
@@ -27,58 +25,16 @@ def freq_search(word, _dict):
                 _dict.insert(index, tmp)
                 index += 1
 
-    res = []
+    res = None
     for i in range(len(_dict)):
         if word == _dict[i][0]:
-            res.append(_dict[i][1])
+            res = _dict[i][1]
+            break
 
-    if len(res) == 0:
+    if not res:
         print("Слово не найдено")
     else:
-        print("Слово было найдено в позициях = ", res)
-
-# def binary_search(word, _dict):
-#     print('> Алгоритм бинарного поиска')
-#
-#     _dict = sorted(_dict)
-#
-#     alph = [0]*33
-#     for i in range(len(_dict)):
-#         alph[ord(_dict[i][0][0]) - 1040] += 1
-#
-#     first = 0
-#     while (word[0] != _dict[first][0][0]):
-#         first += 1
-#
-#     last = first + alph[ord(word[0]) - 1040] - 1
-#
-#     mid = (first + last) // 2
-#
-#     while _dict[mid][0] != word and first < last:
-#         if word > _dict[mid][0]:
-#             first = mid + 1
-#         else:
-#             last = mid - 1
-#         mid = (first + last) // 2
-#
-#     if first > last:
-#         print("Слово не найдено")
-#     else:
-#         res = []
-#         tmp = mid-1
-#         while _dict[mid][0][1] == word[1]:
-#             if _dict[mid][0] == word:
-#                 res.append(_dict[mid][1])
-#             mid += 1
-#         mid = tmp
-#         while _dict[mid][0][1] == word[1]:
-#             if _dict[mid][0] == word:
-#                 res.append(_dict[mid][1])
-#             mid -= 1
-#         if len(res) == 0:
-#             print("Слово не найдено")
-#         else:
-#             print("Слово было найдено в позициях = ", res)
+        print("Слово было найдено в позиции = ", res)
 
 class Tree:
     def __init__(self, value, index, left = None, right = None):
@@ -90,28 +46,21 @@ class Tree:
     def __str__(self):
         return str(self.value)
 
-def print_tree(tree):
-    if tree == None: return
-    print(tree.value, tree.index)
-    print_tree(tree.left)
-    print_tree(tree.right)
 
-
-def balance_search(tree, word):
-    if tree == None: return False
-    if tree.value == word:
-        res = []
-        res.append(tree.index)
-        tmp = tree.left
-        while tmp != None and tmp.value == word:
-            res.append(tmp.index)
-            tmp = tmp.left
+def balance_search(tree, word, res):
+    if tree == None:
         return res
-
-    if word < tree.value:
-        return balance_search(tree.left, word)
     else:
-        return balance_search(tree.right, word)
+        if word < tree.value:
+            res = balance_search(tree.left, word, res)
+        elif word > tree.value:
+            res = balance_search(tree.right, word, res)
+        else:
+            res = tree.index
+        return res
+        # balance_search(tree.left, word, res)
+        # balance_search(tree.right, word, res)
+
 
 def build_tree(array):
     if len(array) == 1:
@@ -119,19 +68,23 @@ def build_tree(array):
     mid = len(array) // 2
 
     if len(array) != 2:
-        return Tree(array[mid][0], array[mid][1], build_tree(array[:mid]), build_tree(array[mid+1:]))
+        return Tree(array[mid][0], array[mid][1], build_tree(array[:mid]), \
+        build_tree(array[mid+1:]))
     else:
         return Tree(array[mid][0], array[mid][1], build_tree(array[:mid]))
 
 def tree_search(word, _dict):
     print('> Поиск по дереву')
+
     _dict = sorted(_dict)
     tree = build_tree(_dict)
-    result = balance_search(tree, word)
-    if result == False:
+
+    res = None
+    res = balance_search(tree, word, res)
+    if not res:
         print("Слово не найдено")
     else:
-        print("Слово было найдено в позициях = ", result)
+        print("Слово было найдено в позиции = ", res)
 
 def main():
     _dict = []
@@ -159,8 +112,8 @@ def main():
         return
 
     freq_search(word, _dict)
-    # binary_search(word, _dict)
     tree_search(word, _dict)
+
 
 if __name__ == "__main__":
     main()
